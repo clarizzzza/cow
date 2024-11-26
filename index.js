665cd15fbe4057b85201753750cf2e88f6d64aa9
insurancefraud <153699839+clarizzzza@users.noreply.github.com>
2024-11-26 15:52:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-
 const image = document.getElementById('image');
+const clickCountElement = document.getElementById('click-count');
+
+
+let clickCount = 0;
+
 
 image.addEventListener('click', () => {
+  
     image.classList.toggle('enlarged');
+
+  
+    clickCount++;
+
+
+    clickCountElement.textContent = clickCount;
 });
