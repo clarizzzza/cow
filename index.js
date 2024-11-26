4bbc100c5af555a3957d044ebb3c76735b56d3d2
insurancefraud <153699839+clarizzzza@users.noreply.github.com>
2024-11-26 15:34:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// Select the image element
+
 const image = document.getElementById('image');
 
-// Add a click event listener to the image
 image.addEventListener('click', () => {
-    // Toggle the 'enlarged' class on click
     image.classList.toggle('enlarged');
 });
